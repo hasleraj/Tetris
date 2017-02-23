24eed857867180c66dc413c9824b55970e7707c1
Ashley Hasler <ashleyhasler@Ashleys-MacBook-Pro.local>
2017-02-23 10:44:07 -0400

    Add show/hide game screen functions

--- GameScreen.js
+++ GameScreen.js
@@ -4,5 +4,26 @@
 /*jshint esversion: 6 */
 var GameScreen = function (assetManager, stage) {
 
+    //CustomEvent
+    var eventScreenComplete = new CustomEvent("instructionsFinished");
+
+    //construct container object
+    var screen = new createjs.Container();
+
+
+
+    //------------------------------public methods
+    this.showMe = function () {
+        stage.addChild(screen);
+    };
+
+    this.hideMe = function () {
+        stage.removeChild(screen);
+    };
+
+    //-----------------------------event handlers
+    function onClick(e) {
+        stage.dispatchEvent(eventScreenComplete);
+    }
 
 };
